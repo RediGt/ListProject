9ac0b79b076bae8a57a0e58f74e2ef79cc86065a
RediGt <red.i@inbox.lv>
2020-07-31 20:18:32 +0300

    Ver_07.31_01

--- ListProject/Program.cs
+++ ListProject/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
 
 namespace ListProject
 {
     class Program
     {       
+        static int timesModified = 0;
+        static DateTime dtModified = new DateTime();
+        
         static void Main(string[] args)
         {
+                       
             List<String> menu = new List<String>();
             string userChoice = null;
-            
-            InitMenu(menu);
+
+            LoadMenu(menu);
+            //InitMenu(menu);
             PrintMenu(menu);
 
             while (userChoice != "q" && userChoice != "Q")
@@ -27,6 +34,7 @@ namespace ListProject
                         break;
                     case "q":
                     case "Q":
+                        SaveMenu(menu);
                         break;
                     default:
                         Console.WriteLine("Incorrect input.\n");
@@ -36,11 +44,12 @@ namespace ListProject
             }
         }
 
+        /*
         static void InitMenu(List<string> menu)
         {
             menu.Add("New");
             menu.Add("Exit");
-        }
+        }*/
 
         static void AddString(List<string> menu)
         {
@@ -48,6 +57,8 @@ namespace ListProject
             string menuStr = Console.ReadLine();
             menu.Add(menuStr);
             Console.WriteLine();
+            timesModified++;
+            dtModified = DateTime.Now;
         }
 
         static void DeleteString(List<string> menu)
@@ -68,6 +79,8 @@ namespace ListProject
             while (!correctInput);
 
             menu.RemoveAt(Convert.ToInt32(menuStr) - 1);
+            timesModified++;
+            dtModified = DateTime.Now;
         }
 
         static void PrintMenu(List<string> menu)
@@ -89,6 +102,74 @@ namespace ListProject
             Console.Write("Make your choice: ");
 
             return Console.ReadLine();
+        }
+
+        static void SaveMenu(List<string> menu)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+ 
+            XmlWriter xmlOut =  XmlWriter.Create(GetMenuFile(), settings);
+
+            xmlOut.WriteStartDocument();
+            xmlOut.WriteComment("File for storage content menu.");
+
+            xmlOut.WriteStartElement("Menu");
+           
+            xmlOut.WriteAttributeString("Times_Modified", Convert.ToString(timesModified));
+            xmlOut.WriteAttributeString("Last_Modified", dtModified.ToString());
+
+            foreach (var item in menu)
+            {
+                xmlOut.WriteStartElement("MenuItem");
+                xmlOut.WriteString(item);
+                xmlOut.WriteEndElement();
+            }
+                
+            xmlOut.WriteEndElement();
+            xmlOut.WriteEndDocument(); 
+            xmlOut.Flush();
+            xmlOut.Close();
+        }
+
+        static void LoadMenu(List<string> menu)
+        {
+            XmlReader xmlIn = XmlReader.Create(GetMenuFile());
+            bool firstEncounter = true;
+
+            while (xmlIn.Read())
+            {
+                if (xmlIn.LocalName.Equals("Menu") && firstEncounter)
+                {
+                    string attribut;
+                    attribut = xmlIn.GetAttribute("Times_Modified");
+                    Console.WriteLine("Times modified: {0}", attribut);
+                    timesModified = Convert.ToInt32(attribut);
+
+                    attribut = xmlIn.GetAttribute("Last_Modified");
+                    Console.WriteLine("Last modified: {0}", attribut);
+                    dtModified = Convert.ToDateTime(attribut);
+
+                    Console.WriteLine();
+                    firstEncounter = false;
+                }
+
+                if (xmlIn.LocalName.Equals("MenuItem"))
+                {
+                    xmlIn.Read();
+                    menu.Add(xmlIn.Value);
+                    xmlIn.Read();
+                }
+            }
+            xmlIn.Close();
+        }
+
+        public static string GetMenuFile()
+        {
+            string filename = Directory.GetCurrentDirectory();
+            filename += @"\Menu.xsd";
+
+            return filename;
         }
     }
 }
